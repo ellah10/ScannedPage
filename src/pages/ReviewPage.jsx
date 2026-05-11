ea473931fa8c0f85d4bc14fe13757d41bbb93bf9
ellah <ellahjr11@gmail.com>
2026-05-11 11:09:25 +0000

    reviewPage changes

--- src/pages/ReviewPage.jsx
+++ src/pages/ReviewPage.jsx
@@ -1,4 +1,5 @@
 import "./ReviewPage.scss";
+import logo from "../assets/AVILogo.webp"
 
 const ReviewPage = () => {
 
@@ -6,40 +7,34 @@ const ReviewPage = () => {
 
     window.location.href =
       "https://search.google.com/local/writereview?placeid=ChIJGeHl8ffhIxAR8JGF08VNM80",
-        "_blank";
+      "_blank";
   };
 
   return (
-
     <div className="review-page">
-
-      <div className="review-card">
-
-        <div className="review-logo">
-          ⭐
+      <div className="reviewContainer">
+        <div className="heading">
+          <img src={logo} alt="logo" />
+          <h3>L'unique caution financiere étudiante certifiée ISO 9001</h3>
         </div>
-
-        <h1>
-          Votre avis compte
-        </h1>
-
-        <p>
-          Merci de prendre quelques secondes pour partager votre expérience.
-        </p>
-
-        <div className="stars">
-          ★★★★★
+        <div className="content">
+          <h2>Votre avis compte</h2>
+          <div className="review-btns">
+            <button onClick={handleReview}>
+              Laisser un avis
+            </button>
+            <button className="blue">
+              Suivez nous sur Nos réseaux
+            </button>
+            <button className="black">
+              Nos Horaires
+            </button>
+            <button className="blue">
+              Nos Services
+            </button>
+          </div>
         </div>
-
-        <button
-          className="review-btn"
-          onClick={handleReview}
-        >
-          Laisser un avis sur Google
-        </button>
-
       </div>
-
     </div>
   );
 };
